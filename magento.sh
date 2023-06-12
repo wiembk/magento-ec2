f9582db8a747b9b7012883ea97753c386ef60ab2
Wièm Boubakri <75736358+wiembk@users.noreply.github.com>
2023-06-12 10:39:41 +0100

    Update magento.sh

--- magento.sh
+++ magento.sh
@@ -20,18 +20,14 @@ EsUser=$(aws cloudformation list-exports --query "Exports[?Name=='magento-es-EsM
 EsPassword=$(aws cloudformation list-exports --query "Exports[?Name=='magento-es-EsMasterUserPassword'].Value" --output text --region eu-central-1)
 RedisServer=$(aws cloudformation list-exports --query "Exports[?Name=='magento-ec-ElastiCachePrimaryEndpoint'].Value" --output text --region eu-central-1)
 RedisPort=$(aws cloudformation list-exports --query "Exports[?Name=='magento-ec-ElastiCacheClusterPort'].Value" --output text --region eu-central-1)
-AccessKey='AKIA4PZTVEZPOLAZTGJK'
-SecretKey='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'
 #BucketName='webkul-s3extension'
 Region='eu-central-1'
 PublicKey='49d1c54d206e19340755129627d96bf6'
 PrivateKey='db060a47cee75868043aa97359427ccf'
+
 #################### Update and install Apache
 sudo apt update
-sudo apt-get install expect -y
-
 sudo apt install apache2 -y
-
 ######################### Check Apache version and enable it
 systemctl is-enabled apache2
 
@@ -47,31 +43,26 @@ sudo apt install mysql-server -y
 #sudo mysql -u root -pwiem2202 -e "GRANT ALL PRIVILEGES ON *.* TO 'magento2'@'localhost' WITH GRANT OPTION;"
 #sudo mysql -u root -pwiem2202 -e "SELECT user,authentication_string,plugin,host FROM mysql.user;"
 #sudo mysql -u magento2 -pwiem2202 -e "CREATE DATABASE magento2;"
-
 ############### configure RDS mysql server
 sudo mysql -u ${DBName} -p${DBPassword} --host ${DBHost} -e "CREATE DATABASE magento2;"
 
 ################## Update and install PHP 7.4
 sudo apt update
 sudo apt install php7.4 libapache2-mod-php php-mysql -y
-
 ######################## Replace index.html with index.php and vice versa
 sudo sed -i 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.cgi index.pl index.html index.xhtml index.htm/g' /etc/apache2/mods-enabled/dir.conf
-
 ########################" Install required PHP modules
 sudo apt install php7.4-mbstring -y
 sudo phpenmod mbstring
 sudo a2enmod rewrite
 sudo apt install php7.4-bcmath php7.4-intl php7.4-soap php7.4-zip php7.4-gd php7.4-json php7.4-curl php7.4-cli php7.4-xml php7.4-xmlrpc php7.4-gmp php7.4-common -y
 sudo systemctl reload apache2
-
 ################## Update PHP configuration
 sudo sed -i 's/max_execution_time = 30/max_execution_time = 18000/g' /etc/php/7.4/cli/php.ini
 sudo sed -i 's/max_input_time = 60/max_input_time = 1800/g' /etc/php/7.4/cli/php.ini
 sudo sed -i 's/memory_limit = -1/memory_limit = 2G/g' /etc/php/7.4/cli/php.ini
 
 ############### Install elasticsearch
-
 #sudo apt install curl -y
 #sudo curl -sSfL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --no-default-keyring --keyring=gnupg-ring:/etc/apt/trusted.gpg.d/magento.gpg --import
 #sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
@@ -94,6 +85,8 @@ cd /var/www/html/
 sudo wget https://getcomposer.org/installer -O composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
 composer
+
+################# Install Magento
 sudo chown -R  ubuntu:ubuntu /var/www/html/
 sudo -u ubuntu composer --no-interaction config --global http-basic.repo.magento.com "$PublicKey" "$PrivateKey"
 sudo -u ubuntu composer create-project --no-install --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.3 magento2
@@ -108,7 +101,7 @@ sudo find var generated vendor pub/static pub/media app/etc -type d -exec chmod 
 sudo chown -R ubuntu:www-data .
 sudo chmod u+x bin/magento
 
-################# Install Magento
+################# configure magento
 sudo php bin/magento setup:install --base-url=http://${BaseUrl} --db-host=${DBHost} --db-name=${DBName} --db-user=${DBName} --db-password=${DBPassword} --admin-firstname=Admin --admin-lastname=Admin --admin-email=admin@admin.com --admin-user=admin --admin-password=${DBPassword} --language=en_US --currency=USD --timezone=America/Chicago --backend-frontname=admin --search-engine=elasticsearch7 --elasticsearch-host=https://${EsHost} --elasticsearch-port=${EsPort} --elasticsearch-enable-auth=1 --elasticsearch-username=${EsUser} --elasticsearch-password=${EsPassword}
 
 
